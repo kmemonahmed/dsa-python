371a98b816d3158786ed99dbd72a7ce219832b86
K M Emon Ahmed <emon@ticonsys.com>
2023-07-24 17:47:43 +0600

    function based insetion sort code example

---
+++ sorting/insertion-sort/insertion-sort-function-based.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        item = arr[i]
+        j =  i - 1
+
+        while j >= 0 and arr[j] > item:
+            arr[j+1] = arr[j]
+            j = j - 1
+
+        arr[j+1] = item
+
+    return (arr)
+
+unsorted_arr = [5, 2, 1, 7, 6, 8, 3, 4, 9, 0, 10, 7, 14, 14, 16, 13, 11]
+sorted_arr = insertion_sort(unsorted_arr)
+print(sorted_arr)
