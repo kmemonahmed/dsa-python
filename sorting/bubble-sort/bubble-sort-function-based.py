c35ff000c58a8dc0485036365d214a43a2934604
K M Emon Ahmed <emon@ticonsys.com>
2023-07-24 15:15:48 +0600

    added bullble sort function based code example

---
+++ sorting/bubble-sort/bubble-sort-function-based.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+unsorted_arr = [5, 2, 1, 7, 6, 8, 3, 4, 9, 0, 10, 7, 14, 14, 16, 13, 11]
+sorted_arr = bubble_sort(unsorted_arr)
+print(sorted_arr)
