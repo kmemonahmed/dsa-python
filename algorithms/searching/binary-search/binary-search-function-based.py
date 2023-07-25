3853684477a7a0b7113677e36aeab85e9d3f3834
K M Emon Ahmed <emon@ticonsys.com>
2023-07-25 16:14:37 +0600

    binary search function based example added

---
+++ algorithms/searching/binary-search/binary-search-function-based.py
@@ -0,0 +1,19 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    target = 4
+    print(binary_search(arr, target))
