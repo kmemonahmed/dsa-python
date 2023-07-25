e1c59d6f73d0ad2b2aae2afb8bdfd202d62d011e
K M Emon Ahmed <emon@ticonsys.com>
2023-07-25 16:25:52 +0600

    added stack implementation with python list example

---
+++ data-structure/stack/stack-with-list.py
@@ -0,0 +1,16 @@
+def stack_with_list():
+    stack = []
+    stack.append(1)
+    stack.append(2)
+    stack.append(3)
+    print(stack)
+    stack.pop()
+    print(stack)
+    stack.pop()
+    print(stack)
+    stack.pop()
+    print(stack)
+
+
+if __name__ == "__main__":
+    stack_with_list()
