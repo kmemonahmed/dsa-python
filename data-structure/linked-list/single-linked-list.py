d08f076d4950a3ee9fe70ecbd81fb5efed8a40d6
K M Emon Ahmed <emon@ticonsys.com>
2023-08-06 13:10:45 +0600

    added linked list travarse and print

--- data-structure/linked-list/single-linked-list.py
+++ data-structure/linked-list/single-linked-list.py
@@ -7,6 +7,17 @@ class SingleLinkedList:
     def __init__(self):
         self.head = None
 
+    def print_linked_list(self):
+        list_str = ""
+        to_travarse = self.head
+        while to_travarse:
+            list_str+=f"{to_travarse.val}->"
+            to_travarse = to_travarse.next
+
+        list_str+= str(None)
+        print(list_str)
+        
+
     def insert_at_begining(self, data):
         if self.head:
             data.next = self.head
@@ -16,5 +27,11 @@ class SingleLinkedList:
 
 
 list_1 = SingleLinkedList()
+list_1.print_linked_list()
 data = Node(1)
 list_1.insert_at_begining(data)
+list_1.print_linked_list()
+
+data=Node(2)
+list_1.insert_at_begining(data)
+list_1.print_linked_list()
