5ab2ada9502f03c59c2859e54145607ef9ba0d12
K M Emon Ahmed <emon@ticonsys.com>
2023-08-06 13:09:47 +0600

    linked list inset at beginning

--- data-structure/linked-list/single-linked-list.py
+++ data-structure/linked-list/single-linked-list.py
@@ -7,6 +7,14 @@ class SingleLinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_at_begining(self, data):
+        if self.head:
+            data.next = self.head
+            self.head = data
+        else:
+            self.head = data
+
 
 list_1 = SingleLinkedList()
-list_1.head = Node(1)
+data = Node(1)
+list_1.insert_at_begining(data)
