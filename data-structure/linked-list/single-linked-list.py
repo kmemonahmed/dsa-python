e9e9f787d9f1864499b58838304e279274257932
K M Emon Ahmed <emon@ticonsys.com>
2023-08-06 14:39:11 +0600

    Lined list insert in between and find node/element

--- data-structure/linked-list/single-linked-list.py
+++ data-structure/linked-list/single-linked-list.py
@@ -16,9 +16,19 @@ class SingleLinkedList:
 
         list_str+= str(None)
         print(list_str)
+
+    def find_node(self, data):
+        to_find = self.head
+        while to_find:
+            if to_find.val == data:
+                return to_find
+            else:
+                to_find = to_find.next
+        return None
         
 
     def insert_at_begining(self, data):
+        data = Node(data)
         if self.head:
             data.next = self.head
             self.head = data
@@ -26,6 +36,7 @@ class SingleLinkedList:
             self.head = data
 
     def insert_at_end(self, data):
+        data = Node(data)
         if self.head:
             head_copy = self.head
             while head_copy.next:
@@ -34,20 +45,36 @@ class SingleLinkedList:
         else:
             self.head = data
 
+    def insert_in_between(self, middle_node, data):
+        data = Node(data)
+        if middle_node:
+            data.next = middle_node.next
+            middle_node.next = data
+        else:
+            print('Middle node is not present')
+
+
+
+
 list_1 = SingleLinkedList()
 list_1.print_linked_list()
-data = Node(1)
-list_1.insert_at_begining(data)
+
+list_1.insert_at_begining(2)
 list_1.print_linked_list()
 
-data=Node(2)
-list_1.insert_at_begining(data)
+list_1.insert_at_begining(1)
 list_1.print_linked_list()
 
-data = Node(3)
-list_1.insert_at_end(data)
+list_1.insert_at_end(3)
 list_1.print_linked_list()
 
-data = Node(4)
-list_1.insert_at_end(data)
+list_1.insert_at_end(4)
 list_1.print_linked_list()
+
+# insert after 2
+middle_node = list_1.find_node(2)
+if middle_node:
+    list_1.insert_in_between(middle_node, 5)
+    list_1.print_linked_list()
+else:
+    print('Middle node not found')
