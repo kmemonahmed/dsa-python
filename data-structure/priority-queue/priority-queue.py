e8b86aa1a85ba11eb926de1bef0a952fd6ee81b0
K M Emon Ahmed <emon@ticonsys.com>
2023-08-21 10:46:20 +0600

    implemented priority queue

---
+++ data-structure/priority-queue/priority-queue.py
@@ -0,0 +1,37 @@
+class PriorityQueue:
+    def __init__(self):
+        self.queue = []
+
+    def insert(self, data):
+        self.queue.append(data)
+
+    def delete(self):
+        try:
+            max = 0
+            for i in range(len(self.queue)):
+                if self.queue[i] > self.queue[max]:
+                    max = i
+            item = self.queue[max]
+            del self.queue[max]
+            return item
+        except IndexError:
+            print()
+            exit()
+
+    def print(self):
+        for i in self.queue:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    myQueue = PriorityQueue()
+    myQueue.insert(12)
+    myQueue.insert(1)
+    myQueue.insert(14)
+    myQueue.insert(7)
+    myQueue.print()
+    while not len(myQueue.queue) == 0:
+        print(myQueue.delete())
+    myQueue.print()
+    
